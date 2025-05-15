b254d7a86f2ff5a5d4713966820ae93818ece110
Ilona Chupa <ilonachupa@gmail.com>
2025-05-16 00:40:22 +0300

    Update category.js
    add validation for category.js

--- routes/category.js
+++ routes/category.js
@@ -3,10 +3,10 @@ const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/category_controllers');
 
-router.get('/', categoryController.getCategories); // Змінено на getCategories
-router.get('/:id', categoryController.getCategoryById); // Потрібно додати в контролер
-router.post('/', categoryController.createCategory);
-router.put('/:id', categoryController.updateCategory); // Потрібно додати в контролер
-router.delete('/:id', categoryController.deleteCategory);
+router.get('/', categoryController.getAllCategories);
+router.get('/:id', categoryController.validateGetCategoryById, categoryController.getCategoryById);
+router.post('/', categoryController.validateCreateCategory, categoryController.createCategory);
+router.put('/:id', categoryController.validateUpdateCategory, categoryController.updateCategory);
+router.delete('/:id', categoryController.validateDeleteCategory, categoryController.deleteCategory);
 
 module.exports = router;
