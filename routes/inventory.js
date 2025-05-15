8ddf03b71db77abcf5a6266df30befbf616fcbc3
Ilona Chupa <ilonachupa@gmail.com>
2025-05-16 01:04:31 +0300

    Update inventory.js
    add validation for inventory.js

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,11 +1,12 @@
 const express = require('express');
+
 const router = express.Router();
 const inventoryController = require('../controllers/inventory_controllers');
 
-router.get('/', inventoryController.getInventory); // Змінено на getInventory
-router.get('/:id', inventoryController.getInventoryById);
-router.post('/', inventoryController.createInventory);
-router.put('/:id', inventoryController.updateInventory);
-router.delete('/:id', inventoryController.deleteInventory);
+router.get('/', inventoryController.getInventory);
+router.get('/:id', inventoryController.validateGetInventoryById, inventoryController.getInventoryById);
+router.post('/', inventoryController.validateCreateInventory, inventoryController.createInventory);
+router.put('/:id', inventoryController.validateUpdateInventory, inventoryController.updateInventory);
+router.delete('/:id', inventoryController.validateDeleteInventory, inventoryController.deleteInventory);
 
 module.exports = router;
